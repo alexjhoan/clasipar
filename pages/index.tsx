b16ade51a2367d3607f3cabb65531d4de2d3ee20
alexjhoan <alexjhoan.25@gmail.com>
2020-12-01 17:20:40 -0400

    function menor changes 1720 01122020

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export async function getStaticProps(context) {
 }
 
 const index = ({data}) => {
-	const [rows, setRows] = useState([['ad_id','user_id','user_email','ad_supplier','ad_cellphone_number','ad_phone_number']])
+	const [rows, setRows] = useState([['ad_id','corporate_name','user_id','user_email','ad_supplier','ad_location','ad_cellphone_number','ad_cellhopne_number2','ad_phone_number']])
 	const [activeBtn, setActiveBtn] = useState({
 		btnDownload: false,
 		btnConvert: true
@@ -28,10 +28,13 @@ const index = ({data}) => {
 		data.list.map((item) => {
 			rows.push([
 				item.ad_id,
+				item.corporate_name,
 				item.user_id,
 				item.user_email,
 				item.ad_supplier,
+				item.ad_location.map(i => {return(i.location_name)}).join(" - "),
 				item.ad_metadata.ad_cellphone_number ? item.ad_metadata.ad_cellphone_number.value : '',
+				item.ad_metadata.ad_cellhopne_number2 ? item.ad_metadata.ad_cellhopne_number2.value : '',
 				item.ad_metadata.ad_phone_number ? item.ad_metadata.ad_phone_number.value : ''
 			])
 		})
@@ -49,7 +52,7 @@ const index = ({data}) => {
 		const encodedUri = encodeURI(csvContent);
 		const link = document.createElement("a");
 		link.setAttribute("href", encodedUri);
-		link.setAttribute("download", "my_data.csv");
+		link.setAttribute("download", "Clasipar.csv");
 		document.body.appendChild(link);
 		link.click();
 	}
